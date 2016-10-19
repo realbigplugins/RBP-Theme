5829d77db760a31eac7cb843aee59dc55258d36e
Eric Defore <eric.defore@gmail.com>
2016-10-19 12:55:14 -0400

    Get some of this up here for history now that it is cleaner

--- single-download.php
+++ single-download.php
@@ -13,83 +13,222 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 get_header();
 the_post();
+
+// Don't show the add to cart stuff here as well as the sidebar. Reduntant
+remove_action( 'the_content', 'edd_after_download_content', 10 );
+
 ?>
 
-<div class="page-content row">
+<div class="downloads-header download-color-section">
+    
+    <h1 class="title"><span itemprop="name"><?php the_title(); ?></span></h1>
+    
+    <?php the_post_thumbnail( 'medium' ); ?>
+    
+</div>
 
-	<div class="row product-row">
+<?php if ( $documentation = get_post_meta( get_the_ID(), '_rbm_p2p_documentation', true ) ) : ?>
+    <div class="documentation">
+        <a class="button large documentation-link" href="<?php echo get_permalink( $documentation ); ?>">
+            <?php printf( __( 'Get started with %s now!', THEME_ID ), get_the_title() ); ?>
+        </a>
+    </div>
+<?php endif; ?>
+
+<?php			
+                
+$requirements = rbm_get_field( 'requirements' );
+
+if ( ! empty( $requirements ) ) : 
+
+$index = 1;
+
+switch( count( $requirements ) ) {
+
+    case 1 :
+        $column_class = 'medium-12';
+        break;
+    case 2 : 
+        $column_class = 'medium-6';
+    default : 
+        $column_class = 'medium-4';
+        break;
+
+}
+
+$features = rbm_get_field( 'features' );
+
+if ( ! empty( $features ) ) : ?>
+
+    <div class="features-section">
+
+        <?php 
+
+        $index = 0;
+
+        foreach ( $features as $feature ) : ?>
         
-        <div class="product-title columns small-12 hide-for-medium hide-for-large">
-			<h1 class="title"><span itemprop="name"><?php the_title(); ?></span></h1>
-		</div>
+            <div class="feature">
 
-		<div class="product-image columns small-12 medium-4">
-			<?php the_post_thumbnail( 'large' ); ?>
+                <div class="row">
 
-			<div class="product-actions">
-				<?php if ( ! edd_has_variable_prices( get_the_ID() ) ) { ?>
-					<h4 class="single-product-price"><?php edd_price( get_the_ID() ); ?></h4>
-				<?php } ?>
+                    <div class="small-12 columns">
 
-				<?php echo edd_get_purchase_link( array(
-                    'download_id' => get_the_ID(), 
-                    'text' => 'Add to Cart', 
-                    'class' => 'primary',
-                 ) ); ?>
-                
-			</div>
-		</div>
+                    <?php if ( $index % 2 == 0 ) : ?>
 
-		<div class="product-content columns small-12 medium-8">
-
-			<h1 class="title hide-for-small-only"><?php the_title(); ?></h1>
-
-			<div class="product-content content">
-				<?php
-				// Don't show the add to cart stuff here as well as the sidebar. Reduntant
-				remove_action( 'the_content', 'edd_after_download_content', 10 );
-				the_content( 'Read the rest of this entry &raquo;' );
-                
-                $requirements = rbm_get_field( 'requirements' );
-
-                if ( ! empty( $requirements ) ) : 
-
-                    $index = 1;
-
-                    switch( count( $requirements ) ) {
-
-                        case 1 :
-                            $column_class = 'medium-12';
-                            break;
-                        case 2 : 
-                            $column_class = 'medium-6';
-                        default : 
-                            $column_class = 'medium-4';
-                            break;
-
-                    }
-
-                    ?>
-
-                    <div class="requirements row">
-                        <div class="small-12 columns">
-
-                            <h5><?php _e( 'Requirements', THEME_ID ); ?></h5>
-
-                            <ul class="multi-column">
-
-                                <?php foreach( $requirements as $item ) : ?>
-
-                                    <li class="small-12 <?php echo $column_class; ?> columns">
-                                        <?php echo $item['requirement']; ?>
-                                    </li>
-
-                                <?php endforeach; ?>
-
-                            </ul>
-
-                        </div>
+                    <div class="small-6 medium-6 columns text-center">
+                        <a href="<?php echo wp_get_attachment_url( $feature['image'], 'full' ); ?>">
+                            <?php echo wp_get_attachment_image( $feature['image'], 'medium' ); ?>
+                        </a>
                     </div>
+
+                    <?php endif; ?>
+
+                    <div class="small-6 medium-6 columns">
+                        <h3><?php echo $feature['title']; ?></h3>
+                        <?php echo apply_filters( 'the_content', $feature['content'] ); ?>
+                    </div>
+
+                    <?php if ( $index % 2 !== 0 ) : ?>
+
+                    <div class="small-6 medium-6 columns text-center">
+                        <a href="<?php echo wp_get_attachment_url( $feature['image'], 'full' ); ?>">
+                            <?php echo wp_get_attachment_image( $feature['image'], 'medium' ); ?>
+                        </a>
+                    </div>
+
+                    <?php endif; ?>
+
+                    </div>
+                </div>
+                
+            </div>
+
+            <?php
+
+            $index++;
+
+        endforeach; ?>
+        
+    </div>
+
+<?php endif; ?>
+
+<?php $testimonials = rbm_get_field( 'testimonials' );
+
+if ( ! empty( $testimonials ) ) : ?>
+
+    <?php
+
+    $index = 1;
+
+    switch( count( $testimonials ) ) {
+
+        case 1 :
+            $max_columns = 1;
+            break;
+        default : 
+            $max_columns = 2;
+            break;
+
+    }
+
+    $image_column_class = 'small-4';
+    $column_class = 'small-8';
+
+    ?>
+
+    <div class="testimonials-section">
+
+    <?php foreach ( $testimonials as $testimonial ) : ?>
+
+        <?php if ( $index == 1 ) : ?>
+            <div class="row">
+                <div class="small-12 columns">
+        <?php endif; ?>
+
+            <div class="testimonial small-12 medium-6<?php echo ( count( $testimonials ) == 1 ) ? ' medium-offset-3' : ' columns'; ?>">
+                
+                <div class="testimonial-container">
+
+                    <div class="<?php echo $image_column_class; ?> columns">
+                        <?php echo get_avatar( $testimonial['gravatar_email'] ); ?>
+                    </div>
+
+                    <div class="<?php echo $column_class; ?> columns">
+
+                        <blockquote><?php echo apply_filters( 'the_content', '"' . $testimonial['content'] . '"' ); ?></blockquote>
+
+                        <div class="testimonial-header">
+                            <h5>
+                                <?php echo $testimonial['name']; ?>
+                            </h5>
+                            <?php echo $testimonial['company']; ?>
+                        </div>
+
+                    </div>
+                    
+                </div>
+
+            </div>
+
+        <?php if ( $index == $max_columns ) : ?>
+
+            </div>
+        </div>
+
+        <?php 
+
+            $index = 1;
+
+        else : 
+
+            $index++; 
+
+        endif;
+
+    endforeach;
+
+    if ( $index !== 1 ) : ?>
+
+            </div>
+        </div>
+
+    <?php endif; ?>
+
+    </div>
+
+<?php endif; ?>
+
+<div class="download-buy download-color-section">
+    
+    <?php echo edd_get_purchase_link( array(
+        'download_id' => get_the_ID(), 
+        'text' => sprintf( _x( 'Add %s to your cart', 'Add to Cart Text', THEME_ID ), get_the_title() ),
+        'class' => 'primary large expanded invert',
+     ) ); ?>
+    
+</div>
+
+<div class="requirements row">
+    <div class="small-12 columns">
+
+        <h5><?php _e( 'Requirements', THEME_ID ); ?></h5>
+
+        <ul class="multi-column">
+
+            <?php foreach( $requirements as $item ) : ?>
+
+                <li class="small-12 <?php echo $column_class; ?> columns">
+                    <?php echo $item['requirement']; ?>
+                </li>
+
+            <?php endforeach; ?>
+
+        </ul>
+
+    </div>
+</div>
 
                     <?php
 
@@ -108,160 +247,11 @@ the_post();
 
 	</div>
 
-    <?php $testimonials = rbm_get_field( 'testimonials' );
+    
 
-    if ( ! empty( $testimonials ) ) : ?>
+    
 
-        <?php
-
-        $index = 1;
-        $first = true;
-
-        switch( count( $testimonials ) ) {
-                
-            case 1 :
-                $max_columns = 1;
-                break;
-            default : 
-                $max_columns = 2;
-                break;
-                
-        }
-
-        $image_column_class = 'small-4';
-        $column_class = 'small-8';
-
-        ?>
-
-        <div class="testimonials alternating-branding">
-        
-        <?php foreach ( $testimonials as $testimonial ) : ?>
-        
-            <?php if ( $index == 1 ) : ?>
-                <div class="row">
-                    <div class="small-12 columns">
-            <?php endif; ?>
-                        
-                <?php if ( $first ) : ?>
-                        
-                    <h3><?php _e( 'Testimonials', THEME_ID ); ?></h3>
-                        
-                <?php
-                        
-                    $first = false;
-                        
-                endif ?>
-                    
-                <div class="testimonial-container small-12 medium-6<?php echo ( count( $testimonials ) == 1 ) ? ' medium-offset-3' : ' columns'; ?>">
-        
-                    <div class="<?php echo $image_column_class; ?> columns">
-                        <?php echo get_avatar( $testimonial['gravatar_email'] ); ?>
-                    </div>
-
-                    <div class="<?php echo $column_class; ?> columns">
-                        
-                        <blockquote><?php echo apply_filters( 'the_content', '"' . $testimonial['content'] . '"' ); ?></blockquote>
-                        
-                        <div class="testimonial-header">
-                            <h5>
-                                <?php echo $testimonial['name']; ?>
-                            </h5>
-                            <?php echo $testimonial['company']; ?>
-                        </div>
-                        
-                    </div>
-                    
-                </div>
-                    
-            <?php if ( $index == $max_columns ) : ?>
-                    
-                </div>
-            </div>
-
-            <?php 
-
-                $index = 1;
-
-            else : 
-
-                $index++; 
-
-            endif;
-        
-        endforeach;
-
-        if ( $index !== 1 ) : ?>
-
-                </div>
-            </div>
-
-        <?php endif; ?>
-
-        </div>
-
-    <?php endif;
-
-    $features = rbm_get_field( 'features' );
-
-    if ( ! empty( $features ) ) : ?>
-
-        <div class="features alternating-branding">
-
-        <?php 
-            
-        $index = 0;
-        
-        foreach ( $features as $feature ) : ?>
-
-            <div class="row">
-
-                <?php if ( $index == 0 ) : ?>
-
-                    <h3><?php _e( 'Features', THEME_ID ); ?></h3>
-
-                <?php endif; ?>
-                
-                <div class="small-12 columns">
-
-                <?php if ( $index % 2 == 0 ) : ?>
-
-                <div class="small-6 medium-3 columns text-center">
-                    <a href="<?php echo wp_get_attachment_url( $feature['image'], 'full' ); ?>">
-                        <?php echo wp_get_attachment_image( $feature['image'], 'medium' ); ?>
-                    </a>
-                </div>
-
-                <?php endif; ?>
-
-                <div class="small-6 medium-9 columns">
-                    <h3><?php echo $feature['title']; ?></h3>
-                    <?php echo apply_filters( 'the_content', $feature['content'] ); ?>
-                </div>
-
-                <?php if ( $index % 2 !== 0 ) : ?>
-
-                <div class="small-6 medium-3 columns text-center">
-                    <a href="<?php echo wp_get_attachment_url( $feature['image'], 'full' ); ?>">
-                        <?php echo wp_get_attachment_image( $feature['image'], 'medium' ); ?>
-                    </a>
-                </div>
-
-                <?php endif; ?>
-
-                </div>
-            </div>
-
-            <?php
-
-            $index++;
-        
-        endforeach; ?>
-            
-        </div>
-
-    <?php endif;
-
-    $video_url = rbm_get_field( 'video' );
+    <?php $video_url = rbm_get_field( 'video' );
 
     if ( ! empty( $video_url ) ) : ?>
 
